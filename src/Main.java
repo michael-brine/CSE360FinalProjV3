9c057ec04dbe3035836e8655e11da3437f4311c6
skaruna2 <72311734+skaruna2@users.noreply.github.com>
2020-12-02 19:49:10 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 public class Main {
+    /**
+	 * Constructor for class Main
+	 * 
+	 * @Description: this constructor intialize the teh Repo and world the adds world to repo
+	 *
+	 */
     Main() {
         Repo repo = new Repo();
         World world = new World(repo);
         repo.addObserver(world);
     }
-
+/**
+	 * @description: just create a main object to call everything for the program
+	 * @param: String[] args 
+	 * @return: [static void] 	  
+	 */
     public static void main(String[] args) {
         Main main = new Main();
     }
