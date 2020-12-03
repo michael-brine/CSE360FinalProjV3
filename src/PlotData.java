89318dcf2bea3fca550d05dd0cb02d68786c3817
skaruna2 <72311734+skaruna2@users.noreply.github.com>
2020-12-02 19:54:57 -0800

    Update PlotData.java

--- src/PlotData.java
+++ src/PlotData.java
@@ -10,7 +10,12 @@ import javax.swing.*;
 
 public class PlotData extends JFrame {
     private Repo repo;
-
+/**
+	 * Constructor for class PlotData
+	 * 
+	 * @Description: this intilaize the repository and sets up the chart of the table and deisplays a Jpanel
+	 *
+	 */
     PlotData(Repo repo) {
         this.repo = repo;
         XYSeriesCollection data = new XYSeriesCollection();
@@ -38,11 +43,19 @@ public class PlotData extends JFrame {
         this.setLocation(400, 200);
         this.setVisible(true);
     }
-
+/**
+	 * @description: returns the charPanel frame to the system
+	 * @param:
+	 * @return: [ChartPanel] 	  
+	 */
     public ChartPanel getChartPanel() {
         return getChartPanel();
     }
-
+/**
+	 * @description: the number of student that have specific attendace percentage
+	 * @param: int[] percents , int key
+	 * @return: [int] 	  
+	 */
     int countPercents(int[] percents, int key) {
         int count = 0;
         for (int i = 0; i < percents.length; i++) {
@@ -52,7 +65,11 @@ public class PlotData extends JFrame {
         }
         return count;
     }
-
+/**
+	 * @description: generates the prercent dates for students that attended and specific dates
+	 * @param: 
+	 * @return: [int [][]]  	  
+	 */
     int[][] makePercentsForDates() {
         int[][] percents = new int[repo.getNColumns().size()][repo.getData().length];
 
